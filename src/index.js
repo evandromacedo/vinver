46d7e61701b38fe4651b5d349262d43a296bf09d
Evandro Macedo <evandro.hcm@gmail.com>
2018-05-14 09:54:30 -0300

    Modifica o index.js para conditional import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Routes from 'routes.js';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Routes />, document.getElementById('root'));
+const app = window.location.href.includes('aluno') ? 'aluno' : 'professor';
+
+import(`./app_${app}/containers/App/App`).then((AppImport) => {
+  const App = AppImport.default;
+
+  const Routes = () => (
+    <Router basename="/vinver">
+      <Route path={`/${app}`} component={App} />
+    </Router>
+  );
+
+  ReactDOM.render(<Routes />, document.getElementById('root'));
+});
+
 registerServiceWorker();
