feb92fbcbec8dcab841993da67d769256d634a27
Evandro Macedo <evandro.hcm@gmail.com>
2018-05-14 21:33:54 -0300

    Profile do aluno

---
+++ src/app_aluno/components/Profile/Profile.js
@@ -0,0 +1,70 @@
+// React
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+// Styles
+import './Profile.css';
+
+class Profile extends Component {
+  render() {
+    const {
+      title,
+      subtitle,
+      usuarioImagem,
+      coverImagem,
+      nivel,
+      ranking
+    } = this.props;
+
+    return (
+      <div className="profile perfil-area">
+        <div className="profile__cover-photo" style={{ '--url': `url(${coverImagem})` }} />
+        <div className="profile__info">
+          <div className="profile__info__left">
+            <div style={{ position: 'relative' }}>
+              <img
+                className="profile__info__left__profile-pic cursor-pointer"
+                src={usuarioImagem}
+                alt="Profile"
+              />
+            </div>
+            <div className="profile__info__line-height">
+              <h1 className="title-2 light-primary">{ title }</h1>
+              <h2 className="headline light-disabled">{ subtitle }</h2>
+            </div>
+          </div>
+          <div className="profile__info__line-height">
+            <p className="headline light-primary">Nível</p>
+            <p className="title-2 light-disabled text-right">{ nivel }</p>
+          </div>
+          <div className="profile__info__line-height">
+            <p className="headline light-primary">Ranking</p>
+            <p className="title-2 light-disabled text-right">#{ ranking }</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+Profile.propTypes = {
+  title: PropTypes.string,
+  subtitle: PropTypes.string,
+  nivel: PropTypes.number,
+  ranking: PropTypes.number,
+  projetos: PropTypes.number,
+  usuarioImagem: PropTypes.string,
+  coverImagem: PropTypes.string
+};
+
+Profile.defaultProps = {
+  title: '',
+  subtitle: '',
+  nivel: 0,
+  ranking: 0,
+  projetos: 0,
+  usuarioImagem: '',
+  coverImagem: ''
+};
+
+export default Profile;
