d6cff0f93f34fee8f9ce397ff07f719a5fdce6d5
Evandro Macedo <evandro.hcm@gmail.com>
2018-06-21 17:07:58 -0300

    Exibe classes e alunos nas metas

--- src/app_professor/containers/Metas/MetasDetail.js
+++ src/app_professor/containers/Metas/MetasDetail.js
@@ -1,5 +1,6 @@
 // React
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 // Styles
 import './Metas.css';
@@ -21,9 +22,95 @@ class MetasDetail extends Component {
     return (
       <div className="metas-detail">
         <Meta type="large" {...this.meta} />
+        <MetasInfo linkTo="#" nameInfo="Classes" infos={classesList} />
+        <MetasInfo linkTo="#" nameInfo="Alunos" infos={alunosList} />
       </div>
     );
   }
 }
+
+const MetasInfo = ({ nameInfo, infos, linkTo }) => (
+  <div className="metas-info">
+    <p className="metas-info__title headline dark-primary">{nameInfo}</p>
+    <div className="metas-info__wrap">
+      <div className="metas-info__list">
+        {infos.map((info, index) => (
+          <Link
+            key={index}
+            to={info.linkTo}
+            style={{ '--image-url': `url(${info.image})` }}
+            className="metas-info__list__item"
+          />
+        ))}
+      </div>
+      <Link to={linkTo} className="title-3 dark-secondary">
+        →
+      </Link>
+    </div>
+  </div>
+);
+
+// Mocks
+const classesList = [
+  {
+    image: '',
+    linkTo: '#'
+  },
+  {
+    image: '',
+    linkTo: '#'
+  },
+  {
+    image: '',
+    linkTo: '#'
+  },
+  {
+    image: '',
+    linkTo: '#'
+  },
+  {
+    image: '',
+    linkTo: '#'
+  },
+  {
+    image: '',
+    linkTo: '#'
+  },
+  {
+    image: '',
+    linkTo: '#'
+  }
+];
+
+const alunosList = [
+  {
+    image: 'https://static.publicocdn.com/files/starwars/img/luke/02.jpg',
+    linkTo: '#'
+  },
+  {
+    image: '',
+    linkTo: '#'
+  },
+  {
+    image: '',
+    linkTo: '#'
+  },
+  {
+    image: '',
+    linkTo: '#'
+  },
+  {
+    image: '',
+    linkTo: '#'
+  },
+  {
+    image: '',
+    linkTo: '#'
+  },
+  {
+    image: '',
+    linkTo: '#'
+  }
+];
 
 export default MetasDetail;
