1f7d00b0ea34ba3276e4367dadcc39d679b822d4
Evandro Macedo <evandro.hcm@gmail.com>
2018-07-10 03:28:21 -0300

    Finaliza Profile mobile do aluno e ajustes no container

--- src/app_aluno/components/Profile/Profile.js
+++ src/app_aluno/components/Profile/Profile.js
@@ -7,39 +7,34 @@ import './Profile.css';
 
 class Profile extends Component {
   render() {
-    const {
-      title,
-      subtitle,
-      usuarioImagem,
-      coverImagem,
-      nivel,
-      ranking
-    } = this.props;
+    const { title, subtitle, usuarioImagem, coverImagem, nivel, ranking } = this.props;
 
     return (
       <div className="profile perfil-area">
-        <div className="profile__cover-photo" style={{ '--url': `url(${coverImagem})` }} />
+        <div className="profile__cover-photo" style={{ '--url': `url(${coverImagem})` }}>
+          <img className="profile__image profile__image--mobile cursor-pointer" src={usuarioImagem} alt="Profile" />
+        </div>
         <div className="profile__info">
           <div className="profile__info__left">
-            <div style={{ position: 'relative' }}>
-              <img
-                className="profile__info__left__profile-pic cursor-pointer"
-                src={usuarioImagem}
-                alt="Profile"
-              />
+            <div className="profile__image-wrapper">
+              <img className="profile__image cursor-pointer" src={usuarioImagem} alt="Profile" />
             </div>
             <div className="profile__info__line-height">
-              <h1 className="title-2 light-primary">{ title }</h1>
-              <h2 className="headline light-disabled">{ subtitle }</h2>
+              <h1 className="title-2 light-primary">{title}</h1>
+              <h2 className="headline light-disabled">{subtitle}</h2>
             </div>
+          </div>
+          <div className="profile__info__line-height profile__info__title-mobile">
+            <h1 className="title-2 light-primary">{title}</h1>
+            <h2 className="headline light-disabled">{subtitle}</h2>
           </div>
           <div className="profile__info__line-height">
             <p className="headline light-primary">Nível</p>
-            <p className="title-2 light-disabled text-right">{ nivel }</p>
+            <p className="title-2 light-disabled profile__info__numbers">{nivel}</p>
           </div>
           <div className="profile__info__line-height">
             <p className="headline light-primary">Ranking</p>
-            <p className="title-2 light-disabled text-right">#{ ranking }</p>
+            <p className="title-2 light-disabled profile__info__numbers">#{ranking}</p>
           </div>
         </div>
       </div>
