2160a7112b86a0a508b29ac1b327321df80c641f
Evandro Macedo <evandro.hcm@gmail.com>
2018-07-09 21:04:01 -0300

    Perfil mobile para professor

--- src/app_professor/components/Profile/Profile.js
+++ src/app_professor/components/Profile/Profile.js
@@ -7,44 +7,38 @@ import './Profile.css';
 
 class Profile extends Component {
   render() {
-    const {
-      title,
-      subtitle,
-      usuarioImagem,
-      coverImagem,
-      classes,
-      alunos,
-      projetos
-    } = this.props;
+    const { title, subtitle, usuarioImagem, coverImagem, classes, alunos, projetos } = this.props;
 
     return (
       <div className="profile perfil-area">
-        <div className="profile__cover-photo" style={{ '--url': `url(${coverImagem})` }} />
+        <div className="profile__cover-photo" style={{ '--url': `url(${coverImagem})` }}>
+          <img className="profile__image profile__image--mobile cursor-pointer" src={usuarioImagem} alt="Profile" />
+        </div>
         <div className="profile__info">
           <div className="profile__info__left">
-            <div style={{ position: 'relative' }}>
-              <img
-                className="profile__info__left__profile-pic cursor-pointer"
-                src={usuarioImagem}
-                alt="Profile"
-              />
+            <div className="profile__image-wrapper">
+              <img className="profile__image cursor-pointer" src={usuarioImagem} alt="Profile" />
             </div>
             <div className="profile__info__line-height">
-              <h1 className="title-2 dark-primary">{ title }</h1>
-              <h2 className="headline dark-disabled">{ subtitle }</h2>
+              <h1 className="title-2 dark-primary">{title}</h1>
+              <h2 className="headline dark-disabled">{subtitle}</h2>
             </div>
+          </div>
+          <div className="profile__info__line-height profile__info__title-mobile">
+            <h1 className="title-2 dark-primary">{title}</h1>
+            <h2 className="headline dark-disabled">{subtitle}</h2>
           </div>
           <div className="profile__info__line-height">
             <p className="headline dark-primary">Classes</p>
-            <p className="title-2 dark-disabled text-right">{ classes }</p>
+            <p className="title-2 dark-disabled profile__info__numbers">{classes}</p>
           </div>
           <div className="profile__info__line-height">
             <p className="headline dark-primary">Alunos</p>
-            <p className="title-2 dark-disabled text-right">{ alunos }</p>
+            <p className="title-2 dark-disabled profile__info__numbers">{alunos}</p>
           </div>
           <div className="profile__info__line-height">
             <p className="headline dark-primary">Projetos</p>
-            <p className="title-2 dark-disabled text-right">{ projetos }</p>
+            <p className="title-2 dark-disabled profile__info__numbers">{projetos}</p>
           </div>
         </div>
       </div>
